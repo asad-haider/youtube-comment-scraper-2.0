3390f59ced37caeb9343c94309c30a475cbebc1f
Philip Klostermann <philip.klostermann@gmail.com>
2017-01-11 17:48:58 -0500

    add blueprint.js UI Kit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,16 +3,24 @@ import { IndexLink, Link } from 'react-router'
 import './Header.scss'
 
 export const Header = () => (
-  <div>
-    <h1>React Redux Starter Kit</h1>
-    <IndexLink to='/' activeClassName='route--active'>
-      Home
-    </IndexLink>
-    {' · '}
-    <Link to='/counter' activeClassName='route--active'>
-      Counter
-    </Link>
-  </div>
+  <nav className='pt-navbar pt-dark'>
+    <div className='pt-navbar-group pt-align-left'>
+      <div className='pt-navbar-heading'>Youtube Comment Scraper</div>
+    </div>
+  </nav>
 )
+
+// export const Header = () => (
+//   <div>
+//     <h1>React Redux Starter Kit</h1>
+//     <IndexLink to='/' activeClassName='route--active'>
+//       Home
+//     </IndexLink>
+//     {' · '}
+//     <Link to='/counter' activeClassName='route--active'>
+//       Counter
+//     </Link>
+//   </div>
+// )
 
 export default Header
