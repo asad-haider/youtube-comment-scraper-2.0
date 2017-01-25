2e3ffadf4e9960c20eb7834346fa0afe8e244bad
Philip Klostermann <philip.klostermann@gmail.com>
2017-01-25 18:41:26 -0500

    make columns resizable by default

--- src/routes/Scraper/components/CommentTable/columns.js
+++ src/routes/Scraper/components/CommentTable/columns.js
@@ -5,51 +5,43 @@ export default [
   {
     key: 'id',
     name: 'ID',
-    width: 250,
-    resizable : true
+    width: 250
   },
   {
     key: 'author',
     name: 'Author',
-    width: 150,
-    resizable : true
+    width: 150
   },
   {
     key: 'authorLink',
     name: 'Author Link',
-    width: 150,
-    resizable : true
+    width: 150
   },
   {
     key: 'authorThumb',
     name: 'Author Thumb',
-    width: 150,
-    resizable : true
+    width: 150
   },
   {
     key: 'text',
     name: 'Text',
     width: 300,
-    resizable : true,
     formatter: TextColumnFormatter
   },
   {
     key: 'likes',
     name: 'Likes',
-    width: 50,
-    resizable : true
+    width: 50
   },
   {
     key: 'time',
     name: 'Time',
-    width: 130,
-    resizable : true
+    width: 130
   },
   {
     key: 'timestamp',
     name: 'Timestamp',
-    width: 120,
-    resizable : true
+    width: 120
   },
   {
     key: 'edited',
@@ -62,7 +54,6 @@ export default [
     key: 'hasReplies',
     name: 'Has Replies',
     width: 85,
-    resizable : true,
     formatter: BooleanColumnFormatter
   },
   {
@@ -74,45 +65,38 @@ export default [
   {
     key: 'reply_author',
     name: 'Reply Author',
-    width: 150,
-    resizable : true
+    width: 150
   },
   {
     key: 'reply_authorLink',
     name: 'Reply Author Link',
-    width: 150,
-    resizable : true
+    width: 150
   },
   {
     key: 'reply_authorThumb',
     name: 'Reply Author Thumb',
-    width: 150,
-    resizable : true
+    width: 150
   },
   {
     key: 'reply_text',
     name: 'Reply Text',
     width: 300,
-    resizable : true,
     formatter: TextColumnFormatter
   },
   {
     key: 'reply_likes',
     name: 'Reply Likes',
-    width: 80,
-    resizable : true
+    width: 80
   },
   {
     key: 'reply_time',
     name: 'Reply Time',
-    width: 130,
-    resizable : true
+    width: 130
   },
   {
     key: 'reply_timestamp',
     name: 'Reply Timestamp',
-    width: 120,
-    resizable : true
+    width: 120
   },
   {
     key: 'reply_edited',
@@ -121,4 +105,4 @@ export default [
     resizable: true,
     formatter: BooleanColumnFormatter
   }
-]
+].map(c => ({ resizable: true, ...c }))
