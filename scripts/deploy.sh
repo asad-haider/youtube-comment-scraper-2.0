cf505e8aeeada8644aa01f4bdf2394d71dfbea18
Philip Klostermann <philip.klostermann@gmail.com>
2017-01-30 18:23:09 -0500

    add deploy scripts and deploy info to README

---
+++ scripts/deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#
+# Currently this script is the same as redeploy, but once a database
+# has been added, it will be different
+#
+
+# Pull down new image
+echo "# Pull down new image"
+docker pull philbot/youtube-comment-scraper2.0:latest
+
+# Kill and remove existing container
+echo "# Kill and remove existing container (may fail)"
+docker kill youtube-comment-scraper2.0
+docker rm youtube-comment-scraper2.0
+
+# Run container
+echo "# Start new container"
+docker run -p 49162:3000 -d --name youtube-comment-scraper2.0 philbot/youtube-comment-scraper2.0
---
+++ scripts/redeploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Pull down new image
+echo "# Pull down new image"
+docker pull philbot/youtube-comment-scraper2.0:latest
+
+# Kill and remove existing container
+echo "# Kill and remove existing container (may fail)"
+docker kill youtube-comment-scraper2.0
+docker rm youtube-comment-scraper2.0
+
+# Run container
+echo "# Start new container"
+docker run -p 49162:3000 -d --name youtube-comment-scraper2.0 philbot/youtube-comment-scraper2.0
